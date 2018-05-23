9579a4239e9cd05bccd84197cc2ab218649b4ab6
TaigaMikami <mikamimikami60@gmail.com>
2018-05-24 00:14:12 +0900

    ログアウト

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
   get 'home/about', as: :about
   get 'home/contact', as: :contact
 
-  resources :users
-
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations",
     passwords: "users/passwords",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+
+  resources :users
+  get '/users/sign_out' => 'devise/sessions#destroy'
 
 end
